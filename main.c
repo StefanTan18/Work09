0f48f844b29d57c8a0bc63164f61ae54d69bc381
Trollicorn <muddin3@stuy.edu>
2018-10-23 11:09:52 -0400

    read/write

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -8,6 +11,11 @@ int main() {
 
   int fd = open("test.txt", O_CREAT | O_RDWR);
 
+  write(fd, "hello world", 7);   
+
+  char * c;
+  read(fd, c, 5);
+  printf("%s",c);
 
   return close(fd);
 
